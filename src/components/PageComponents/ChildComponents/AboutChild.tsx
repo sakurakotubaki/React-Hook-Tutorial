b6185373fc5846da1f16bba9d61928d826b64485
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-10-30 17:37:22 +0900

    [change]
    memo化したコードを使用

--- src/components/PageComponents/ChildComponents/AboutChild.tsx
+++ src/components/PageComponents/ChildComponents/AboutChild.tsx
@@ -1,32 +1,7 @@
-import { Button } from "@chakra-ui/react";
-import React, { useState } from "react";
-
-const AboutChild = () => {
-  const [childCount, setChildCount] = useState<number>(0)
-
-  const onClildClick = () => {
-    setChildCount(childCount + 1);
-  }
-  console.log('childコンポーネントが呼ばれた!')
-
-  return (
-    <div>
-      <p>クリック回数:{childCount}</p>
-      <Button colorScheme="red" onClick={onClildClick}>
-        ChildButton
-      </Button>
-    </div>
-  );
-};
-
-export default AboutChild;
-
-// memo化したコンポーネント
-// 不要な再レンダリングを防ぐ
 // import { Button } from "@chakra-ui/react";
-// import React, { memo, useState } from "react";
+// import React, { useState } from "react";
 
-// const AboutChild = memo(() => {
+// const AboutChild = () => {
 //   const [childCount, setChildCount] = useState<number>(0)
 
 //   const onClildClick = () => {
@@ -36,11 +11,36 @@ export default AboutChild;
 
 //   return (
 //     <div>
+//       <p>クリック回数:{childCount}</p>
 //       <Button colorScheme="red" onClick={onClildClick}>
 //         ChildButton
 //       </Button>
 //     </div>
 //   );
-// })
+// };
 
 // export default AboutChild;
+
+// memo化したコンポーネント
+// 不要な再レンダリングを防ぐ
+import { Button } from "@chakra-ui/react";
+import React, { memo, useState } from "react";
+
+const AboutChild = memo(() => {
+  const [childCount, setChildCount] = useState<number>(0)
+
+  const onClildClick = () => {
+    setChildCount(childCount + 1);
+  }
+  console.log('childコンポーネントが呼ばれた!')
+
+  return (
+    <div>
+      <Button colorScheme="red" onClick={onClildClick}>
+        ChildButton
+      </Button>
+    </div>
+  );
+})
+
+export default AboutChild;
--- src/components/PageComponents/ChildComponents/ContactChild.tsx
+++ src/components/PageComponents/ChildComponents/ContactChild.tsx
@@ -1,32 +1,10 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
-
-const ContactChild = () => {
-  console.log('ContactChildがよばれた!')
-  console.log('useCallbackを使ってないので何度も呼ばれる!')
-  console.log('親のコンポーネントが再レンダリングされている!')
-  const voidCallBack = () => {
-    setTimeout(() => {
-      console.log('1秒後にContactChildコンポーネントが呼ばれた!')
-    }, 1000)
-  }
-  return (
-    <div>
-      <Button colorScheme="green" onClick={voidCallBack}>ChildContact</Button>
-    </div>
-  )
-}
-
-export default ContactChild
-
-// useCallbackを使った子コンポーネント
 // import { Button } from '@chakra-ui/react'
-// import React, { useCallback } from 'react'
+// import React from 'react'
 
-// const ContactChild = React.memo(() => {
+// const ContactChild = () => {
 //   console.log('ContactChildがよばれた!')
-//   console.log('useCallbackは1回しか呼ばれない!')
-
+//   console.log('useCallbackを使ってないので何度も呼ばれる!')
+//   console.log('親のコンポーネントが再レンダリングされている!')
 //   const voidCallBack = () => {
 //     setTimeout(() => {
 //       console.log('1秒後にContactChildコンポーネントが呼ばれた!')
@@ -37,6 +15,28 @@ export default ContactChild
 //       <Button colorScheme="green" onClick={voidCallBack}>ChildContact</Button>
 //     </div>
 //   )
-// })
+// }
 
 // export default ContactChild
+
+// useCallbackを使った子コンポーネント
+import { Button } from '@chakra-ui/react'
+import React, { useCallback } from 'react'
+
+const ContactChild = React.memo(() => {
+  console.log('ContactChildがよばれた!')
+  console.log('useCallbackは1回しか呼ばれない!')
+
+  const voidCallBack = () => {
+    setTimeout(() => {
+      console.log('1秒後にContactChildコンポーネントが呼ばれた!')
+    }, 1000)
+  }
+  return (
+    <div>
+      <Button colorScheme="green" onClick={voidCallBack}>ChildContact</Button>
+    </div>
+  )
+})
+
+export default ContactChild
